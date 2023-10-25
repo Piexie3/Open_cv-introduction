75bda4976c5633ee9aaaa3cd6c84494663c91216
Emmanuel Bett <emmanuelkbett3@gmail.com>
2023-10-25 16:10:51 +0300

    initial commit

---
+++ getFromComputer.py
@@ -0,0 +1,10 @@
+"""
+If you don't have a camera, you can also use the 'imread' command to get images from files:
+"""
+
+import cv2
+
+frame = cv2.imread ("C:\\Users\\Bett\\Downloads\\flutter.jpg")
+
+cv2.imshow("loaded image", frame) # display it
+cv2.waitKey(-1) # Wait indefinitely until the user presses a key.
---
+++ getFromMp4.py
@@ -0,0 +1,21 @@
+"""
+You can also load videos from a video source, like so:
+"""
+
+import cv2 
+
+cam = cv2.VideoCapture("C:\\Users\\Bett\\Downloads\\coding.mp4")
+
+isOK=True
+while isOK:
+    isOK, frame = cam.read()
+    if isOK:
+        cv2.imshow("video frame", frame)
+        cv2.waitKey(1) 
+
+
+"""
+You may notice the video is a lot faster that you would be seeing if you watched it. That's because openCV 
+doesnt need to care about the framerate of the original video. You can tweak the waitKey until the video matches 
+the framerate you want to process at.
+"""
